7c05fd1864b0284427ef85852640d4be328eb003
Marco Rojo <mrmarcondes@gmail.com>
2012-04-08 22:00:24 -0300

    pesquisa de shoppings por localizacao

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 ShopFinder::Application.routes.draw do
   
   match 'shoppings' => 'shoppings#all', :via => :get
-  match 'shoppings/:id' => 'shoppings#id', :via => :get
-  match 'location' => 'shoppings#location', :via => :get
+  match 'shoppings/by_id/:id' => 'shoppings#id', :via => :get
+  match 'shoppings/by_location' => 'shoppings#location', :via => :get
   match 'shoppings/:shopping_id/stores' => 'stores#all_by_shopping', :via => :get
 
   root :to => "home#index"
